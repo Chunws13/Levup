ab5652c90a4e73d292f1522a3c327e25bc46b239
chunws <dntkdn51@naver.com>
2023-12-04 17:52:37 +0900

    FE 로그인 페이지 css

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import {Cookies} from "react-cookie";
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Login() {
+const Login = () => {
   const SubmitFunc = (event) => {
     event.preventDefault();
     const cookie = new Cookies();
@@ -9,7 +11,7 @@ function Login() {
     const password = event.target.pw.value;
     
     const postData = {id, password, "email": "test@naver.com"};
-
+    console.log(postData)
     axios.post("http://127.0.0.1:8000/api/users/login", postData)
         .then(response => {
             cookie.set("token", response.data.token)
@@ -18,18 +20,35 @@ function Login() {
   }
 
   return (
-    <div>
-        <div> Welcome! </div>
-        <form onSubmit={SubmitFunc}>
-            <div>
-                <input type='text' name='id' placeholder='ID'/> 
-            </div>
-            <div>
-                <input type='password' name='pw' placeholder='Password'/>
-            </div>
-            <button type="submit"> sumbit </button>
-        </form>
-    </div>
+    <Container className="d-flex align-items-center justify-content-center" 
+        style={{ minHeight: '100vh'}}>
+        <div className="border border-warning border-3 rounded-3 p-5">
+            <p className="text-center"> Project. Levup </p>
+            <Form onSubmit={SubmitFunc}>
+
+                <Form.Group className="mb-2" controlId="formbasicEmail" >
+                    <Form.Label> ID </Form.Label>
+                    <Form.Control type="text" name='id'/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label> Password </Form.Label>
+                    <Form.Control type="password" name="pw"/>
+
+                </Form.Group>
+                
+                <div className="d-grid gap-2">
+                    <Button type="submit" variant="primary">
+                        로그인
+                    </Button>
+
+                    <Button variant="dark">
+                        회원가입
+                    </Button>
+                </div>
+                
+            </Form>
+        </div>
+    </Container>
   )
 }
 
